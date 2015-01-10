e6b8cf6cd25c28739f634ca7592ad8269b7d8b38
Sinya <grace5334@gmail.com>
2015-01-10 12:54:49 +0800

    add fb friend photo

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,5 +3,12 @@ class FriendsController < ApplicationController
     graph = Koala::Facebook::API.new(current_user.oauth_token)
     u = graph.get_object("me")
     @friends = graph.get_connections(u["id"],"friends")
+
+    @my_photo = graph.get_picture(u["id"])
+
+
+	@friends_photo = graph.get_connections("me", "friends?fields=id,name,picture.type(large)")
+
+
   end
 end
